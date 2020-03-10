3e86609a31e1bba2e3f6f30931ef70aa1a67e51c
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-03-10 19:54:31 -0400

    add title to featured rooms

--- src/Context.js
+++ src/Context.js
@@ -1,19 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import items from "./data";
 
 const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
-    state={
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
+  };
+  // getData
 
-    }
-    render() {
-        return( <RoomContext.Provider value={"hello"}>
-            {this.props.children}
-        </RoomContext.Provider>
-        )
-    }
+  componentDidMount() {
+    // this.getData
+    let rooms = this.formatData(items);
+    console.log(rooms);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+
+      let room = { ...item.fields, images: images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
+  render() {
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
 }
 
 const RoomConsumer = RoomContext.Consumer;
 
-export{RoomProvider, RoomConsumer, RoomContext}
+export { RoomProvider, RoomConsumer, RoomContext };
