912326884f57a92cd8c3179f9be69fb583e57dbc
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-03-25 11:25:15 -0400

    add env development

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
 import{createClient} from 'contentful'
 
 export default createClient({
-    space: "r7xjuz3w7pa7" ,
-    accessToken: "ATLSANwqGI_eGIbLkqYaidGWPjfQYENiAWrsuiTDfio"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
